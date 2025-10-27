f426c65a3ccdc6091bae5e0dbc64fe447f5cd26a
faisaldp123 <faisal.dpathshala@gmail.com>
2025-10-27 15:11:49 +0530

    made some more changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon.svg" type="image/png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
